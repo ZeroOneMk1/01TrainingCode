1cb479dd489026b07ba075e6e5d09d13d18106d9
Yusuf Simsek <yusuftariksimsek@gmail.com>
2021-01-22 18:30:43 +0800

    polishing fundamental bal functions

--- Discord Bot/bot.py
+++ Discord Bot/bot.py
@@ -216,6 +216,7 @@ async def rollStats(ctx):
     stats.sort()
     await ctx.send(f'Your stats, in ascending order, are: {stats}')
 
+
 @client.command()
 async def rollHP(ctx, lvl, dice, conmod):
     sum = int(dice) + int(conmod)
@@ -309,7 +310,7 @@ def getIfKylePartyTime():
         return False
 
 
-@client.command(aliases = ['clear',  'erase', 'purge'])
+@client.command(aliases=['clear',  'erase', 'purge'])
 async def delete(ctx, amount=1):
     await ctx.channel.purge(limit=amount+1)
 
@@ -338,9 +339,7 @@ async def map(ctx):
 # @client.command()
 # async def setMap(ctx, map):
 #     premap = open('01TrainingCode\Discord Bot\map.png', 'w')
-#     premap.write(map) 
-
-
+#     premap.write(map)
 
 
 @client.command()
@@ -391,37 +390,44 @@ async def karma(ctx):
 
     karma = users[str(ctx.author.id)]
 
-    em = discord.Embed(title = f"{ctx.author.name}, this is your karma.", color = discord.Colour.magenta())
-    em.add_field(name = "Karma", value = karma)
+    em = discord.Embed(
+        title=f"{ctx.author.name}, this is your karma.", color=discord.Colour.magenta())
+    em.add_field(name="Karma", value=karma)
 
-    await ctx.send(embed = em)
+    await ctx.send(embed=em)
+
 
 @client.command()
 async def makeMeRich(ctx, bal):
     await open_account(ctx.author)
 
-    users = await get_bank_data()
-
-    karma = users[str(ctx.author.id)]
     try:
-       karma += int(bal)
-
-       await ctx.send(f"Your new karma is {karma}.")
+        pog = int(bal)
     except:
-        await ctx.send("Fuck off")
+        await ctx.send("Fuck off.")
+        return
 
-    users[str(ctx.author.id)] = karma
+    if(ctx.author.id == 154979334002704384):
+        await add_karma(ctx, bal)
 
-    with open("01TrainingCode/Discord Bot/main.json", 'w') as f:
-        json.dump(users, f)
-    
-    
+    else:
+        await ctx.send("You're not my dad!")
+
+
+@client.command()
+async def beg(ctx):
+    await open_account(ctx.author)
+
+    scraps = rd.randint(1, 100)
+
+    await add_karma(ctx, scraps)
+    await ctx.send("Peasant...")
 
 
 async def open_account(user):
 
     users = await get_bank_data()
-    
+
     if str(user.id) in users:
         return False
     else:
@@ -431,9 +437,26 @@ async def open_account(user):
         json.dump(users, f)
         return True
 
+
 async def get_bank_data():
     with open("01TrainingCode/Discord Bot/main.json", 'r') as f:
         users = json.load(f)
     return users
+
+
+async def add_karma(ctx, amount):
+    users = await get_bank_data()
+
+    karma = users[str(ctx.author.id)]
+
+    karma += int(amount)
+
+    users[str(ctx.author.id)] = karma
+
+    await ctx.send(f"Your new karma is {karma}")
+
+    with open("01TrainingCode/Discord Bot/main.json", 'w') as f:
+        json.dump(users, f)
+
 
 client.run(cfile.read())
