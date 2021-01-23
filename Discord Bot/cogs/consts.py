59671aa2085217d486978b951c2173a47fcb8f15
Yusuf Simsek <yusuftariksimsek@gmail.com>
2021-01-23 21:56:22 +0800

    deleting irrelevant constants

--- Discord Bot/cogs/consts.py
+++ Discord Bot/cogs/consts.py
@@ -1,11 +1,7 @@
 import discord
 from discord.ext import commands
 
-cfile = open('01TrainingCode/Discord Bot/code.txt', 'r')
 status = ["it's time!", "it's not time yet..."]
 classes = ['Artificer', 'Blood Hunter', 'Bard', 'Barbarian', 'Cleric', 'Druid',
            'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard']
 guilds = [792311682898460693, 769850094353776654]
-wizchannels = [792311682898460693, 769850094353776654]
-schedstrings = ['01TrainingCode/Discord Bot/schedule.txt',
-                '01TrainingCode/Discord Bot/schedule copy.txt']
