892beffd90d16a8eb4f7bc585f677af497f70894
Yusuf Simsek <yusuftariksimsek@gmail.com>
2022-12-05 13:42:40 -0500

    added linear motion to Robot

--- Hexapod/main.py
+++ Hexapod/main.py
@@ -4,17 +4,20 @@
 #Libraries
 import time    #https://docs.python.org/fr/3/library/time.html
 from Robot import Robot
+from datetime import datetime as dt
+import numpy as np
 
 # function init 
 def init():
-    robot = Robot()
-    robot.control_board.test_servos()
+    # robot.control_board.test_servos()
+    pass
 
 
 # function main 
 def main():
-    pass
+    robot = Robot()
 
+    robot.move_leg_from_point_to_point(np.array[10, 10, -5], np.array(10, -10, -5), 3)
 
 if __name__ == '__main__':
     init()
