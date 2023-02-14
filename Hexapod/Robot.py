9463139d2fc28621d4d7a56ad8a4a360900ff21e
ZeroOne <43883294+ZeroOneMk1@users.noreply.github.com>
2023-02-14 11:03:05 -0500

    Fixed multiple leg movement bugs

--- Hexapod/Robot.py
+++ Hexapod/Robot.py
@@ -3,17 +3,21 @@ from ControlBoard import ControlBoard
 import numpy as np
 from time import sleep
 from datetime import datetime as dt
+from datetime import timedelta
 import math
 
 THETA_WEIGHT = 0.2
-POINTS_WEIGHT = 1
-COM_WEIGHT = 1
+POINTS_WEIGHT = 0.2
+COM_WEIGHT = 0.5
 
 class Robot:
     def __init__(self, hip_x, hip_y) -> None:
         self.legs = []
         self.hip_x = hip_x
         self.hip_y = hip_y
+
+        self.WORKING_HEIGHT = -11.5
+        self.STANDARD_DISTANCE = 15
 
         self.legs.append(Leg(0, np.array([-hip_x, hip_y, 0]), 135)) # Front Left Leg
         self.legs.append(Leg(1, np.array([-hip_x, 0, 0]), 180)) # Center Left Leg
@@ -22,8 +26,8 @@ class Robot:
         self.legs.append(Leg(4, np.array([hip_x, 0, 0]), 0)) # Center Right Leg
         self.legs.append(Leg(5, np.array([hip_x, -hip_y, 0]), 315)) # Back Right Leg
 
-        self.current_leg_positions = np.asarray([(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)])
-        self.next_leg_positions = np.asarray([(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)])
+        self.current_leg_positions = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        self.next_leg_positions = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
         self.left_tripod = [self.legs[0], self.legs[2], self.legs[4]]
         self.right_tripod = [self.legs[1], self.legs[3], self.legs[5]]
@@ -47,7 +51,7 @@ class Robot:
         """Goes from self.current to self.next positions, time is in seconds"""
 
         start = dt.now()
-        end = start + dt.timedelta(seconds=time)
+        end = start + timedelta(seconds=time)
         now = dt.now()
 
         # self.move_leg_to_position(leg.index, destination)
@@ -56,6 +60,7 @@ class Robot:
         substeps = 0
 
         while now < end:
+
             substeps += 1
 
             for leg in self.legs:
@@ -68,6 +73,9 @@ class Robot:
                 t_sec = t.total_seconds()
                 destination = np.asarray(np.asarray(self.current_leg_positions[leg.index]) + line * t_sec / time)
                 self.move_leg_to_position(leg.index, destination)
+            
+            now = dt.now()
+
         
         for leg in self.legs:
             self.move_leg_to_position(leg.index, self.next_leg_positions[leg.index])
@@ -75,14 +83,30 @@ class Robot:
         print(f"Number of substeps: {substeps}")
 
     def reset_leg_positions(self):
-        self.reset_lifted_tripod()
-        self.swap_tripods()
-        self.reset_lifted_tripod()
+        self.lift_all_legs()
+        sleep(2)
+        self.drop_grounded_legs()
+
+    def lift_all_legs(self):
+        diag_dist = self.STANDARD_DISTANCE * np.sqrt(2)/2
+        self.move_leg_to_position(0, [-self.hip_x - diag_dist,  self.hip_y + diag_dist, 5])
+        self.move_leg_to_position(1, [-self.hip_x - self.STANDARD_DISTANCE,          0, 5])
+        self.move_leg_to_position(2, [-self.hip_x - diag_dist, -self.hip_y - diag_dist, 5])
+        self.move_leg_to_position(3, [ self.hip_x + diag_dist,  self.hip_y + diag_dist, 5])
+        self.move_leg_to_position(4, [ self.hip_x + self.STANDARD_DISTANCE,          0, 5])
+        self.move_leg_to_position(5, [ self.hip_x + diag_dist, -self.hip_y - diag_dist, 5])
+
+        for leg in self.legs:
+            self.current_leg_positions[leg.index] = leg.position
+
+    def drop_grounded_legs(self):
+        self.calculate_new_leg_positions((0, 0))
+        self.move_legs_from_current_to_next(2)
 
     def tick(self):
         controller_input = self.get_controller_input()
         self.calculate_new_leg_positions(controller_input)
-        self.move_legs_from_current_to_next()
+        self.move_legs_from_current_to_next(0.02)
         for i in range(len(self.current_leg_positions)):
             self.current_leg_positions[i] = self.next_leg_positions[i] # ! May have errors when next gets changed
         sleep(1/50)
@@ -106,13 +130,13 @@ class Robot:
             return False
         
         for leg in self.lifted_legs:
-            self.shift_leg_position(leg.index, input, d - 50) # TODO Tune d via different functions
+            self.shift_leg_position(leg.index, input, d + self.WORKING_HEIGHT) # TODO Tune d via different functions
         
         return True
     
     def shift_grounded_leg_positions(self, input: tuple) -> None:        
         for leg in self.grounded_legs:
-            self.shift_leg_position(leg.index, input, -50)
+            self.shift_leg_position(leg.index, input, self.WORKING_HEIGHT)
 
     def shift_leg_position(self, leg_index: int,  vector: tuple, height: float) -> None:
         self.next_leg_positions[leg_index][0] = self.current_leg_positions[leg_index][0] + vector[0]
@@ -123,12 +147,13 @@ class Robot:
 
         # TODO Include collision of legs as another distance parameter (IMPORTANT BEFORE FINAL IMPLEMENTATION!) not needed before visualization
 
-        min_dist = self.legs[0].COXA_LENGTH + np.sqrt(self.hip_x**2 + self.hip_y**2)
+        
         points = self.get_grounded_leg_positions()
 
         distances = []
 
         for i in range(len(points)):
+            min_dist = self.grounded_legs[i].COXA_LENGTH + np.sqrt(self.grounded_legs[i].hipPos[0]**2 + self.grounded_legs[i].hipPos[1]**2)
             dist = np.sqrt(points[i][0]**2+points[i][1]**2) - min_dist
             if dist < 0:
                 return -1
@@ -138,7 +163,12 @@ class Robot:
 
             perp_destination, theta = self.grounded_legs[i].relative_to_perpendicular_destination(rel_destination)
 
-            distances.append(THETA_WEIGHT * theta * 180 / np.pi)
+            theta_dist = 30-abs(theta * 180 / np.pi)
+
+            if theta_dist < 0:
+                return -1
+
+            distances.append(THETA_WEIGHT * theta_dist)
 
         v1 = (points[1][0] - points[0][0], points[1][1] - points[0][1])
         v2 = (points[2][0] - points[0][0], points[2][1] - points[0][1])
@@ -172,4 +202,4 @@ class Robot:
     def loosen(self):
         for servo in self.control_board.servos:
             servo.angle=None
-        # pass
+        pass
